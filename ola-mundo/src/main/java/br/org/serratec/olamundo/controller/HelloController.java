6cc30eccfcd37921bf1655d08f56df657bcdea5b
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-03 19:33:42 -0300

    Text UperCase

--- ola-mundo/src/main/java/br/org/serratec/olamundo/controller/HelloController.java
+++ ola-mundo/src/main/java/br/org/serratec/olamundo/controller/HelloController.java
@@ -2,6 +2,7 @@ package br.org.serratec.olamundo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,11 @@ public class HelloController {
     @GetMapping(value="/resposta")
     public String resposta() {
         return "Oque está fazendo aqui meu caro amigo";
+    }
+
+    @GetMapping(value="/converter")
+    public String converter(@RequestParam String name) {
+        return name.toUpperCase();
     }
 
 }
