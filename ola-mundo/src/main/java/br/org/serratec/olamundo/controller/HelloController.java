c2b8c677b4d7efced9b70b75f7ba3e2776adfcd4
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-03 19:21:53 -0300

    maps

--- ola-mundo/src/main/java/br/org/serratec/olamundo/controller/HelloController.java
+++ ola-mundo/src/main/java/br/org/serratec/olamundo/controller/HelloController.java
@@ -1,14 +1,21 @@
 package br.org.serratec.olamundo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class HelloController {
     
     @GetMapping(value="/")
     public String GetMetHodName() {
         return "Ola Mundo VsCode";
+    }
+
+    @GetMapping(value="/resposta")
+    public String resposta() {
+        return "Oque está fazendo aqui meu caro amigo";
     }
 
 }
