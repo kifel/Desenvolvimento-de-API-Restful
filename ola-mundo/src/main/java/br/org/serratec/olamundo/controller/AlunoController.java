35437e8a5d71a96878b09441333271912cca8462
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-03 20:34:21 -0300

    listando na tela

--- ola-mundo/src/main/java/br/org/serratec/olamundo/controller/AlunoController.java
+++ ola-mundo/src/main/java/br/org/serratec/olamundo/controller/AlunoController.java
@@ -3,6 +3,7 @@ package br.org.serratec.olamundo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +16,15 @@ public class AlunoController {
     private static List<Aluno> alunos = new ArrayList<>();
 
     static {
-        alunos.add(new Aluno("123","Kifel", "k@gmail.com"));
-        alunos.add(new Aluno("321","Maria", "a@gmail.com"));
-        alunos.add(new Aluno("124","Jose", "j@gmail.com"));
-        alunos.add(new Aluno("126","Carlos", "c@gmail.com"));
+        alunos.add(new Aluno("123", "Kifel", "k@gmail.com"));
+        alunos.add(new Aluno("321", "Maria", "a@gmail.com"));
+        alunos.add(new Aluno("124", "Jose", "j@gmail.com"));
+        alunos.add(new Aluno("126", "Carlos", "c@gmail.com"));
     }
-    
+
+    @GetMapping
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
 }
