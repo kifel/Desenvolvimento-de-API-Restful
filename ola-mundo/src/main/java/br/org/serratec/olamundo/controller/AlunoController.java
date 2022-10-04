f18be6970a819689c8f6c186e0c52d9042d7ee32
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-03 21:51:54 -0300

    Response Status

--- ola-mundo/src/main/java/br/org/serratec/olamundo/controller/AlunoController.java
+++ ola-mundo/src/main/java/br/org/serratec/olamundo/controller/AlunoController.java
@@ -3,6 +3,7 @@ package br.org.serratec.olamundo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.org.serratec.olamundo.model.Aluno;
@@ -38,6 +40,7 @@ public class AlunoController {
     }
 
     @PostMapping(value = "/cadastro")
+    @ResponseStatus(HttpStatus.CREATED)
     public Aluno inserir(@RequestBody Aluno aluno) {
         alunos.add(aluno);
         return aluno;
