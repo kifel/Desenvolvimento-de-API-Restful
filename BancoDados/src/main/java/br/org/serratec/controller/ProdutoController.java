ebc8fb7d27c78dcac6e49a2c768d599d6394c6f4
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-04 22:10:39 -0300

    delete produtos

--- BancoDados/src/main/java/br/org/serratec/controller/ProdutoController.java
+++ BancoDados/src/main/java/br/org/serratec/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,6 +44,16 @@ public class ProdutoController {
     @ResponseStatus(code = HttpStatus.CREATED)
     public Produto create(@RequestBody Produto produto) {
         return produtoRepository.save(produto);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        if (!produtoRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        produtoRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
