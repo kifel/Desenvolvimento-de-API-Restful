01969d49fba4947d5a22c3e6b05dff5bb7b21fc6
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-04 21:24:42 -0300

    create new produtos on table

--- BancoDados/src/main/java/br/org/serratec/controller/ProdutoController.java
+++ BancoDados/src/main/java/br/org/serratec/controller/ProdutoController.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.org.serratec.model.Produto;
@@ -33,6 +37,12 @@ public class ProdutoController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping(value = "/create")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Produto create(@RequestBody Produto produto) {
+        return produtoRepository.save(produto);
     }
 
 }
