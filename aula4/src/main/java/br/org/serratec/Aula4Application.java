2a5345757b3f830e14ac3c7558be695b4649aa68
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-05 18:55:09 -0300

    criando novo projeto

---
+++ aula4/src/main/java/br/org/serratec/Aula4Application.java
@@ -0,0 +1,13 @@
+package br.org.serratec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula4Application.class, args);
+	}
+
+}
