dae28dd92153a26c58e1038bd92dcd1b1d7c544d
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-04 21:10:02 -0300

    buscando produto por id

--- BancoDados/src/main/java/br/org/serratec/controller/ProdutoController.java
+++ BancoDados/src/main/java/br/org/serratec/controller/ProdutoController.java
@@ -1,9 +1,12 @@
 package br.org.serratec.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +16,23 @@ import br.org.serratec.repository.ProdutoRepository;
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
-    
+
     @Autowired
     private ProdutoRepository produtoRepository;
 
     @GetMapping
     public List<Produto> listar() {
         return produtoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> buscar(@PathVariable Long id) {
+        Optional<Produto> produto = produtoRepository.findById(id);
+        if (produto.isPresent()) {
+            return ResponseEntity.ok(produto.get());
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
 }
