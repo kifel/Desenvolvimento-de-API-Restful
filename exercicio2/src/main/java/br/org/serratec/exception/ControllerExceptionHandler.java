c09d175ff3fa90e92afc2a4deb4a3228ea8d37c2
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-06 19:53:00 -0300

    tratamento de erro

--- exercicio2/src/main/java/br/org/serratec/exception/ControllerExceptionHandler.java
+++ exercicio2/src/main/java/br/org/serratec/exception/ControllerExceptionHandler.java
@@ -1,10 +1,13 @@
 package br.org.serratec.exception;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -15,8 +18,12 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
             HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<String> erros = new ArrayList<>();
+        for (FieldError erro : ex.getBindingResult().getFieldErrors()) {
+            erros.add(erro.getField() + ":" + erro.getDefaultMessage());
+        }
         ErroResposta erroResposta = new ErroResposta(status.value(),
-                "Existem campos invalidos. Confira o preenchimento", LocalDateTime.now());
+                "Existem campos invalidos. Confira o preenchimento", LocalDateTime.now(), erros);
 
         return super.handleExceptionInternal(ex, erroResposta, headers, status, request);
     }
