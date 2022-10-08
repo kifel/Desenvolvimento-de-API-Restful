c171d04079f8c76dc4e607441cb605579adfc091
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-07 21:28:08 -0300

    tratamento de erro

--- aula-06-h2/src/main/java/br/org/serratec/exception/ControllerExceptionHandler.java
+++ aula-06-h2/src/main/java/br/org/serratec/exception/ControllerExceptionHandler.java
@@ -7,12 +7,12 @@ import java.util.List;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 
 @ControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
@@ -26,6 +26,16 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
         }
         ErroResposta erroResposta = new ErroResposta(status.value(),
                 "Existem campos inválidos. Confira o preenchimento", LocalDateTime.now(), erros);
+
+        return super.handleExceptionInternal(ex, erroResposta, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+                
+        ErroResposta erroResposta = new ErroResposta(status.value(),
+                "Existem campos inválidos. Confira o preenchimento", LocalDateTime.now(), null);
 
         return super.handleExceptionInternal(ex, erroResposta, headers, status, request);
     }
