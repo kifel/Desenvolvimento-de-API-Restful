c55c9be012d82d345361e7b1b5ea9c5193b66655
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-07 22:19:06 -0300

    fiz Enums

--- exercicio2/src/main/java/br/org/serratec/enums/Estados.java
+++ exercicio2/src/main/java/br/org/serratec/enums/Estados.java
@@ -5,30 +5,30 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import br.org.serratec.exception.EnumValidationException;
 
 public enum Estados {
-    AC("Acre"), AL("Alagoas"), AP("Amapá"), AM("Amazonas"), BA("Bahia"), CE("Ceará"), ES("Espírito Santo"), GO("Goiás"),
-    MA("Maranhão"), MT("Mato Grosso"), MS("Mato Grosso do Sul"), MG("Minas Gerais"), PA("Pará"), PB("Paraíba"),
-    PR("Paraná"), PE("Pernambuco"), PI("Piauí"), RJ("Rio de Janeiro"), RN("Rio Grande do Norte"),
-    RS("Rio Grande do Sul"), RO("Rondônia"), RR("Roraima"), SC("Santa Catarina"), SP("São Paulo"), SE("Sergipe"),
-    TO("Tocantins"), DF("Distrito Federal");
+    ACRE("AC"), AlAGOAS("AL"), AMAPÁ("AP"), AMAZONAS("AM"), BAHIA("BA"), CEARÁ("CE"), ESPÍRITO_SANTO("ES"), GOIÁS("GO"),
+    MARANHÃO("MA"), MATO_GROSSO("MT"), MATO_GROSSO_DO_SUL("MS"), MINAS_GERAIS("MG"), PARÁ("PA"), PARAÍBA("PB"),
+    PARANÁ("PR"), PERNAMBUCO("PE"), PIAUÍ("PI"), RIO_DE_JANEIRO("RJ"), RIO_GRANDE_DO_NORTE("RN"),
+    RIO_GRANDE_DO_SUL("RS"), RONDÔNIA("RO"), RORAIMA("RR"), SANTA_CATARINA("SC"), SÃO_PAULO("SP"), SERGIPE("SE"),
+    TOCANTINS("TO"), DISTRITO_FEDERAL("DF");
 
-    private String tipo;
+    private String sigla;
 
-    private Estados(String tipo) {
-        this.tipo = tipo;
+    private Estados(String sigla) {
+        this.sigla = sigla;
     }
 
-    public String getTipo() {
-        return tipo;
+    public String getSigla() {
+        return sigla;
     }
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
     }
 
     @JsonCreator
     public static Estados verifica(String valor) throws EnumValidationException {
         for (Estados estados : Estados.values()) {
-            if (valor.equals(estados.name())) {
+            if (valor.equals(estados.getSigla())) {
                 return estados;
             }
         }
