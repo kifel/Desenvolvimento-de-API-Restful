80fbd1282b81890266821b44f756bd68612041b2
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-11 19:05:10 -0300

    SINGLE_TABLE

--- aula-8/src/main/java/br/org/serratec/model/Fornecedor.java
+++ aula-8/src/main/java/br/org/serratec/model/Fornecedor.java
@@ -1,11 +1,15 @@
 package br.org.serratec.model;
 
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
-
-@MappedSuperclass
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo_fornecedor")
+@Entity
 public class Fornecedor {
 
     @Id
