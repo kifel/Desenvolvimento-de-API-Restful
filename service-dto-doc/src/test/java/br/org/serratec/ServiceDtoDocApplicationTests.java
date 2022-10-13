c9c9b65cc881ee5cce6dd2e77a906c838e45f741
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-13 18:23:44 -0300

    comecando novo projeto

---
+++ service-dto-doc/src/test/java/br/org/serratec/ServiceDtoDocApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.serratec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDtoDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
