cc10a39319b8885437cb0ed5d2926a8cd995e1a0
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-13 19:56:04 -0300

    Controller Expection

---
+++ service-dto-doc/src/main/java/br/org/serratec/exception/ControllerExceptionHandler.java
@@ -0,0 +1,16 @@
+package br.org.serratec.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler  extends ResponseEntityExceptionHandler {
+    
+    @ExceptionHandler(EmailException.class)
+    public ResponseEntity<Object> handleEmailException(EmailException ex) {
+        EmailException emailException = new EmailException(ex.getMessage());
+        return ResponseEntity.unprocessableEntity().body(emailException);
+    }
+}
