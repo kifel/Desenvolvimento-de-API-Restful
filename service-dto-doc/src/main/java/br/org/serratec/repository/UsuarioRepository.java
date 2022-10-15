f46d1c11d2cdf75e5f9cdd79ff0e70f06cc1b274
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-15 11:07:00 -0300

    findByName

--- service-dto-doc/src/main/java/br/org/serratec/repository/UsuarioRepository.java
+++ service-dto-doc/src/main/java/br/org/serratec/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package br.org.serratec.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.org.serratec.model.Usuario;
@@ -7,5 +9,7 @@ import br.org.serratec.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     
     Usuario findByEmail(String email);
+
+    Optional<Usuario> findByNome(String nome);
     
 }
