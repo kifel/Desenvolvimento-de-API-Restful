68c58a042d71874683b83edf0ddc6413033afe0a
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-17 18:19:58 -0300

    modificando arquivos e adicionando mail

--- service-dto-doc/src/main/java/br/org/serratec/exception/ControllerExceptionHandler.java
+++ service-dto-doc/src/main/java/br/org/serratec/exception/ControllerExceptionHandler.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ControllerExceptionHandler  extends ResponseEntityExceptionHandler {
-    
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler(EmailException.class)
     public ResponseEntity<Object> handleEmailException(EmailException ex) {
         EmailException emailException = new EmailException(ex.getMessage());
