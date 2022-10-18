d72ad223505061881f7959a7d466a3839a6e3443
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-18 20:09:55 -0300

    swagger

--- service-dto-doc/src/main/java/br/org/serratec/config/SwaggerConfig.java
+++ service-dto-doc/src/main/java/br/org/serratec/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package br.org.serratec.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import br.org.serratec.model.Perfil;
+import br.org.serratec.model.Usuario;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -20,6 +22,7 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("br.org.serratec.controller"))
                 .paths(PathSelectors.any())
                 .build()
+                .ignoredParameterTypes(Usuario.class, Perfil.class)
                 .apiInfo(apiInfo());
     }
 
--- service-dto-doc/src/main/java/br/org/serratec/dto/EnderecoDTO.java
+++ service-dto-doc/src/main/java/br/org/serratec/dto/EnderecoDTO.java
@@ -1,6 +1,7 @@
 package br.org.serratec.dto;
 
 import br.org.serratec.model.Endereco;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,11 +11,14 @@ import lombok.Setter;
 @NoArgsConstructor
 public class EnderecoDTO {
 
+    @ApiModelProperty(value = "Identifica de uma cep")
     private String cep;
+    @ApiModelProperty(value = "Identifica uma rua")
     private String logradouro;
     private String complemento;
     private String bairro;
     private String localidade;
+    @ApiModelProperty(value = "Identifica o estado")
     private String uf;
 
     public EnderecoDTO(Endereco endereco) {
