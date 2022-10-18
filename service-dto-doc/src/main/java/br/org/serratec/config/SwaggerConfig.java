9bffe0279fb5e46965e33ba3656d71176fb7a945
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-18 19:25:01 -0300

    documentando cep

--- service-dto-doc/src/main/java/br/org/serratec/config/SwaggerConfig.java
+++ service-dto-doc/src/main/java/br/org/serratec/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package br.org.serratec.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -13,10 +16,23 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("br.org.serratec.controller"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo() {
+        ApiInfo info = new ApiInfoBuilder()
+                .title("API DO SERRATEC")
+                .description("Essa API foi desenvolvida pelos alunos")
+                .license("Apache License, Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .termsOfServiceUrl("/service.html")
+                .version("1.0.1")
+                .contact(new Contact("Serratec", "www.serratec.org.br", "serratec@serratec.org.br"))
                 .build();
+        return info;
     }
 }
--- service-dto-doc/src/main/java/br/org/serratec/controller/EnderecoController.java
+++ service-dto-doc/src/main/java/br/org/serratec/controller/EnderecoController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.org.serratec.dto.EnderecoDTO;
 import br.org.serratec.service.EnderecoService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/enderecos")
@@ -18,6 +21,14 @@ public class EnderecoController {
     private EnderecoService enderecoService;
 
     @GetMapping("/{cep}")
+    @ApiOperation(value = "Busca um cep", notes = "preencha com um cep")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna o cep do cliente"),
+            @ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+            @ApiResponse(responseCode = "403", description = "Você não tem permissão para o recurso"),
+            @ApiResponse(responseCode = "404", description = "Cep não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro na aplicação")
+    })
     public ResponseEntity<EnderecoDTO> buscar(@PathVariable String cep) {
         EnderecoDTO dto = enderecoService.buscar(cep);
 
