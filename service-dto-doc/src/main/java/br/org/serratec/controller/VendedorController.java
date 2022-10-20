32085b5f126bcec102812a84926e8f6004d68cd0
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-20 19:20:27 -0300

    filtro na paginação

--- service-dto-doc/src/main/java/br/org/serratec/controller/VendedorController.java
+++ service-dto-doc/src/main/java/br/org/serratec/controller/VendedorController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +28,7 @@ public class VendedorController {
     }
 
     @GetMapping("/page")
-    public ResponseEntity<Page<Vendedor>> listarPorPagina(Pageable pageable) {
+    public ResponseEntity<Page<Vendedor>> listarPorPagina(@PageableDefault(sort = "nome", direction = Direction.DESC, size = 8) Pageable pageable) {
         return ResponseEntity.ok(vendedorService.listarPorPagina(pageable));
     }
     
