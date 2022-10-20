4a599b0f2852848c0eded1d83c89a2ef2e2bad8f
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-20 19:51:44 -0300

    Query native

--- service-dto-doc/src/main/java/br/org/serratec/controller/VendedorController.java
+++ service-dto-doc/src/main/java/br/org/serratec/controller/VendedorController.java
@@ -10,9 +10,11 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.org.serratec.model.Vendedor;
+import br.org.serratec.repository.VendedorRepository;
 import br.org.serratec.service.VendedorService;
 
 @RestController
@@ -22,14 +24,24 @@ public class VendedorController {
     @Autowired
     private VendedorService vendedorService;
 
+    @Autowired
+    VendedorRepository vendedorRepository;
+
     @GetMapping
     public ResponseEntity<List<Vendedor>> listar() {
         return ResponseEntity.ok(vendedorService.listar());
     }
 
     @GetMapping("/page")
-    public ResponseEntity<Page<Vendedor>> listarPorPagina(@PageableDefault(sort = "nome", direction = Direction.DESC, size = 8) Pageable pageable) {
+    public ResponseEntity<Page<Vendedor>> listarPorPagina(
+            @PageableDefault(sort = "nome", direction = Direction.DESC, size = 8) Pageable pageable) {
         return ResponseEntity.ok(vendedorService.listarPorPagina(pageable));
     }
-    
+
+    @GetMapping("/salario")
+    public ResponseEntity<Page<Vendedor>> buscarPorFaixaSalarial(@RequestParam Double valorMinimo,
+            @RequestParam Double valorMaximo, Pageable pageable) {
+        return ResponseEntity.ok(vendedorRepository.buscarPorFaixaSalarial(valorMinimo, valorMaximo, pageable));
+    }
+
 }
--- service-dto-doc/src/main/java/br/org/serratec/repository/VendedorRepository.java
+++ service-dto-doc/src/main/java/br/org/serratec/repository/VendedorRepository.java
@@ -1,9 +1,16 @@
 package br.org.serratec.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.org.serratec.model.Vendedor;
 
 public interface VendedorRepository extends JpaRepository<Vendedor, Long> {
 
+    // @Query("SELECT v FROM Vendedor v WHERE v.salario >=:valorMinimo AND v.salario<=:valorMaximo ")
+    @Query(nativeQuery = true, value = "SELECT * FROM vendedor WHERE salario BETWEEN :valorMinimo AND :valorMaximo")
+    public Page<Vendedor> buscarPorFaixaSalarial(Double valorMinimo, Double valorMaximo, Pageable pageable);
+    
 }
